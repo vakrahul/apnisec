4b66c0b8ec1fd18be5b7fdba3982fcb52b0c31ee
vakrahul <vakitirahul@gmail.com>
2025-12-24 22:05:57 +0530

    css to scss

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -3,7 +3,7 @@
 import { useState, useEffect } from 'react';
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
-import styles from './navbar.module.scss';
+import styles from './Navbar.module.css';
 
 export default function Navbar() {
   const pathname = usePathname();
