c15f4d152dcf76923886d3c283a30117a3b8aa51
vakrahul <vakitirahul@gmail.com>
2025-12-24 22:09:44 +0530

    Fix casing on navbar scss

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -3,7 +3,7 @@
 import { useState, useEffect } from 'react';
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
-import styles from './Navbar.module.css';
+import styles from './navbar.module.scss';
 
 export default function Navbar() {
   const pathname = usePathname();
